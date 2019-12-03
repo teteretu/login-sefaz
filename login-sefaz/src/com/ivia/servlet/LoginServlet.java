847166ec26e8185c9ada3aec1fe1ef14966c5396
matheus melo <matheus.melo@effetive.com.br>
2019-12-03 14:10:24 -0300

    Login Functionality with mysql integration

---
+++ login-sefaz/src/com/ivia/servlet/LoginServlet.java
@@ -0,0 +1,52 @@
+package com.ivia.servlet;
+
+import java.io.IOException;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ivia.dao.LoginDao;
+import com.ivia.model.LoginBean;
+
+/**
+ * @email Ramesh Fadatare
+ */
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private LoginDao loginDao;
+
+	public void init() {
+		loginDao = new LoginDao();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		LoginBean loginBean = new LoginBean();
+		loginBean.setUsername(username);
+		loginBean.setPassword(password);
+
+		try {
+			if (loginDao.validate(loginBean)) {
+				//HttpSession session = request.getSession();
+				// session.setAttribute("username",username);
+				response.sendRedirect("listUsers.jsp");
+			} else {
+				HttpSession session = request.getSession();
+				session.setAttribute("user", username);
+				response.sendRedirect("login.jsp");
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
